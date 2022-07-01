a2545164df90e66f9b0a7ed485a2bbe3da5105f6
DragonEggBedrockBreaking <antonbhaumik@proton.me>
2022-07-01 20:39:29 +0100

    More config stuff

--- src/main/java/codes/ztereohype/autotechno/AutoTechno.java
+++ src/main/java/codes/ztereohype/autotechno/AutoTechno.java
@@ -5,8 +5,23 @@ import codes.ztereohype.autotechno.client.ClientWrapper;
 import codes.ztereohype.autotechno.config.AutoTechnoConfig;
 import net.fabricmc.api.ModInitializer;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AutoTechno implements ModInitializer {
-    public static AutoTechnoConfig config = AutoTechnoConfig.get(true, true, true);
+    public static AutoTechnoConfig config;
+    static {
+        List<String> endMessageList = new ArrayList<String>() {{
+            add("");
+        }};
+        List<String> startMessageList = new ArrayList<String>() {{
+            add("");
+        }};
+        List<String> killMessageList = new ArrayList<String() {{
+            add("");
+        }};
+        config = AutoTechnoConfig.get(true, true, true, endMessageList, startMessageList, killMessageList);
+    }
     public static ClientWrapper client;
     public static EventDetector detector;
 
--- src/main/java/codes/ztereohype/autotechno/config/AutoTechnoConfig.java
+++ src/main/java/codes/ztereohype/autotechno/config/AutoTechnoConfig.java
@@ -10,7 +10,9 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 
 public class AutoTechnoConfig {
     public static final Path CONFIG_PATH = Paths.get("config/autogg.json");
@@ -20,18 +22,25 @@ public class AutoTechnoConfig {
     public boolean startMessages;
     public boolean killMessages;
 
-    private AutoTechnoConfig(boolean endMessages, boolean startMessages, boolean killMessages) {
+    public List<String> endMessageList = new ArrayList<>();
+    public List<String> startMessageList = new ArrayList<>();
+    public List<String> killMessageList = new ArrayList<>();
+
+    private AutoTechnoConfig(boolean endMessages, boolean startMessages, boolean killMessages, List<String> endMessageList, List<String> startMessageList, List<String> killMessageList) {
         this.endMessages = endMessages;
         this.startMessages = startMessages;
         this.killMessages = killMessages;
+        this.endMessageList = endMessageList;
+        this.startMessageList = startMessageList;
+        this.killMessageList = killMessageList;
     }
 
-    public static AutoTechnoConfig get(boolean endMessages, boolean startMessages, boolean finalMessages) {
+    public static AutoTechnoConfig get(boolean endMessages, boolean startMessages, boolean finalMessages, List<String> endMessageList, List<String> startMessageList, List<String> killMessageList) {
         try {
             if (CONFIG_PATH.toFile().exists()) {
                 return GSON.fromJson(new String(Files.readAllBytes(CONFIG_PATH)), AutoTechnoConfig.class);
             } else {
-                AutoTechnoConfig config = new AutoTechnoConfig(endMessages, startMessages, finalMessages);
+                AutoTechnoConfig config = new AutoTechnoConfig(endMessages, startMessages, finalMessages, endMessageList, startMessageList, killMessageList);
                 new File(FilenameUtils.getPath(CONFIG_PATH.toString())).mkdir();
                 Files.write(CONFIG_PATH, Collections.singleton(GSON.toJson(config)));
                 return config;
