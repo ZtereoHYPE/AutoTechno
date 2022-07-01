a07bf72c07b394d9114f9eac6853e60bd47e40c0
DragonEggBedrockBreaking <antonbhaumik@proton.me>
2022-07-01 23:14:05 +0100

    LazyDFU

--- buildscript/src/main/java/Versions.java
+++ buildscript/src/main/java/Versions.java
@@ -4,4 +4,5 @@ public class Versions {
     public static final String LOADER_VERSION = "0.14.8";
     public static final String MOD_VERSION = "0.1.0";
     public static final String QUILTFLOWER_VERSION = "1.8.1";
+    public static final String LAZYDFU_VERSION = "0.1.3";
 }
