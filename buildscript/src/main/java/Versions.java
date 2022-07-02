a1c9c7ec900e10f0f32108f8233f198a9ff01222
ZtereoHYPE <57519662+ZtereoHYPE@users.noreply.github.com>
2022-07-02 15:09:45 +0200

    bump version + mineplex 2

--- buildscript/src/main/java/Versions.java
+++ buildscript/src/main/java/Versions.java
@@ -2,6 +2,6 @@ public class Versions {
     public static final String MINECRAFT_VERSION = "1.8.9";
     public static final String YARN_VERSION = "1.8.9+build.202203281833";
     public static final String LOADER_VERSION = "0.13.3";
-    public static final String MOD_VERSION = "0.1.0";
+    public static final String MOD_VERSION = "0.1.1";
     public static final String QUILTFLOWER_VERSION = "1.8.1";
 }
