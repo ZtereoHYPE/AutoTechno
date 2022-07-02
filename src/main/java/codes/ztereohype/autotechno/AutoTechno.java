313cc9287a23da7e98e138133f0622d9be835f0e
DragonEggBedrockBreaking <antonbhaumik@proton.me>
2022-07-02 16:33:01 +0100

    IntelliJ Code Cleanup (+ Optimise Imports)

--- src/main/java/codes/ztereohype/autotechno/AutoTechno.java
+++ src/main/java/codes/ztereohype/autotechno/AutoTechno.java
@@ -8,38 +8,38 @@ import net.fabricmc.api.ModInitializer;
 
 public class AutoTechno implements ModInitializer {
     public static AutoTechnoConfig config;
-
-    static {
-        String[] startMessages = new String[] { "Good luck, and may Techno's unmatched skill be with you",
-                                                "RIP Techno, you will be missed.",
-                                                "Let's drop kick some children!",
-                                                "Technoblade never dies!",
-                                                "So, what do you guys know about anarchy?" };
-
-        String[] killMessages = new String[] { "Blood for the Blood God",
-                                               "In the name of techno",
-                                               "This ones for technoblade",
-                                               "Officer, I drop-kicked them in self defense!",
-                                               "This is the second-worst thing to happen to these orphans.",
-                                               "Sometimes it's tough being the best",
-                                               "die nerd (/j)",
-                                               "chin up king, your crown is falling"};
-
-        String[] endMessages = new String[] { "gg e z",
-                                              "good game",
-                                              "Rest in Peace Technoblade",
-                                              "Technoblade never dies",
-                                              "so long nerds",
-                                              "as Sun Tzu wanted" };
-
-        config = AutoTechnoConfig.get(true, true, true, endMessages, startMessages, killMessages);
-    }
-
     public static ClientWrapper client;
     public static EventDetector detector;
     public static MessageRandomiser messageRandomiser;
 
-    @Override public void onInitialize() {
+    static {
+        String[] startMessages = new String[]{"Good luck, and may Techno's unmatched skill be with you",
+                "RIP Techno, you will be missed.",
+                "Let's drop kick some children!",
+                "Technoblade never dies!",
+                "So, what do you guys know about anarchy?"};
+
+        String[] killMessages = new String[]{"Blood for the Blood God",
+                "In the name of techno",
+                "This ones for technoblade",
+                "Officer, I drop-kicked them in self defense!",
+                "This is the second-worst thing to happen to these orphans.",
+                "Sometimes it's tough being the best",
+                "die nerd (/j)",
+                "chin up king, your crown is falling"};
+
+        String[] endMessages = new String[]{"gg e z",
+                "good game",
+                "Rest in Peace Technoblade",
+                "Technoblade never dies",
+                "so long nerds",
+                "as Sun Tzu wanted"};
+
+        config = AutoTechnoConfig.get(true, true, true, endMessages, startMessages, killMessages);
+    }
+
+    @Override
+    public void onInitialize() {
         client = new ClientWrapper();
         detector = new EventDetector(config);
         messageRandomiser = new MessageRandomiser(config);
