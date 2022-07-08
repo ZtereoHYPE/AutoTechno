dd0d36028ceece427fccca02b96755459d5bede6
DragonEggBedrockBreaking <antonbhaumik@proton.me>
2022-07-08 19:01:01 +0100

    Add config updater

--- src/main/java/codes/ztereohype/autotechno/config/AutoTechnoConfig.java
+++ src/main/java/codes/ztereohype/autotechno/config/AutoTechnoConfig.java
@@ -15,23 +15,28 @@ public class AutoTechnoConfig {
     private static final File CONFIG_FILE = Paths.get("config/autotechno.yml").toFile();
     private static Map<String, Object> config = new LinkedHashMap<>();
 
-    public static void init(boolean sendEndMessages,
-                                       boolean sendStartMessages,
-                                       boolean sendKillMessages,
-                                       int messageWaitTime,
-                                       String[] endMessages,
-                                       String[] startMessages,
-                                       String[] killMessages) {
+    public static void init(int configVersion,
+                            boolean sendEndMessages,
+                            boolean sendStartMessages,
+                            boolean sendKillMessages,
+                            int messageWaitTime,
+                            String[] endMessages,
+                            String[] startMessages,
+                            String[] killMessages) {
         try {
             if (CONFIG_FILE.exists()) {
                 YamlReader reader = new YamlReader(new FileReader(CONFIG_FILE));
                 config = (Map<String, Object>) reader.read();
                 reader.close();
+                if (getProperty("ConfigVersion") == null || Integer.parseInt((String) getProperty("ConfigVersion")) != configVersion) {
+                    updateConfig(configVersion, sendEndMessages, sendStartMessages, sendKillMessages, messageWaitTime, endMessages, startMessages, killMessages);
+                }
             } else {
                 CONFIG_FILE.getParentFile().mkdirs();
                 CONFIG_FILE.createNewFile();
                 YamlWriter writer = new YamlWriter(new FileWriter(CONFIG_FILE));
                 Map<String, Object> tempConfig = new LinkedHashMap<>();
+                tempConfig.put("ConfigVersion", configVersion);
                 tempConfig.put("SendEndMessages", sendEndMessages);
                 tempConfig.put("SendStartMessages", sendStartMessages);
                 tempConfig.put("SendKillMessages", sendKillMessages);
@@ -41,7 +46,7 @@ public class AutoTechnoConfig {
                 tempConfig.put("KillMessages", killMessages);
                 writer.write(tempConfig);
                 writer.close();
-                init(sendEndMessages, sendStartMessages, sendKillMessages, messageWaitTime, endMessages, startMessages, killMessages);
+                init(configVersion, sendEndMessages, sendStartMessages, sendKillMessages, messageWaitTime, endMessages, startMessages, killMessages);
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
@@ -50,5 +55,34 @@ public class AutoTechnoConfig {
 
     public static Object getProperty(String property) {
         return config.get(property);
+    }
+
+    private static void updateConfig(int configVersion,
+                                     boolean sendEndMessages,
+                                     boolean sendStartMessages,
+                                     boolean sendKillMessages,
+                                     int messageWaitTime,
+                                     String[] endMessages,
+                                     String[] startMessages,
+                                     String[] killMessages) {
+        try {
+            CONFIG_FILE.delete();
+            CONFIG_FILE.createNewFile();
+            YamlWriter writer = new YamlWriter(new FileWriter(CONFIG_FILE));
+            Map<String, Object> tempConfig = new LinkedHashMap<>();
+            tempConfig.put("ConfigVersion", configVersion);
+            tempConfig.put("SendEndMessages", getProperty("SendEndMessages") == null ? sendEndMessages : getProperty("SendEndMessages"));
+            tempConfig.put("SendStartMessages", getProperty("SendStartMessages") == null ? sendStartMessages : getProperty("SendEndMessages"));
+            tempConfig.put("SendKillMessages", getProperty("SendKillMessages") == null ? sendKillMessages : getProperty("SendEndMessages"));
+            tempConfig.put("MessageWaitTime", getProperty("MessageWaitTime") == null ? messageWaitTime : getProperty("MessageWaitTime"));
+            tempConfig.put("EndMessages", getProperty("EndMessages") == null ? endMessages : getProperty("EndMessages"));
+            tempConfig.put("StartMessages", getProperty("StartMessages") == null ? startMessages : getProperty("StartMessages"));
+            tempConfig.put("KillMessages", getProperty("KillMessages") == null ? killMessages : getProperty("KillMessages"));
+            writer.write(tempConfig);
+            writer.close();
+            init(configVersion, sendEndMessages, sendStartMessages, sendKillMessages, messageWaitTime, endMessages, startMessages, killMessages);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
