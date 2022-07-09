7017ba223170d2f54721a7f7185013fb5aa209f8
DragonEggBedrockBreaking <antonbhaumik@proton.me>
2022-07-09 22:11:12 +0100

    Remove unused mod version field

--- buildscript/src/main/java/Versions.java
+++ buildscript/src/main/java/Versions.java
@@ -1,8 +1,8 @@
 public class Versions {
+<<<<<<< HEAD
     public static final String MINECRAFT_VERSION = "1.8.9";
     public static final String YARN_VERSION = "1.8.9+build.202203281833";
     public static final String LOADER_VERSION = "0.13.3";
-    public static final String MOD_VERSION = "0.1.1";
     public static final String QUILTFLOWER_VERSION = "1.8.1";
     public static final String YAML_VERSION = "1.15";
 }
