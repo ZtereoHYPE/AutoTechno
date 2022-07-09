7bd4459720f101b547899acf3c72005f543eff22
DragonEggBedrockBreaking <antonbhaumik@proton.me>
2022-07-09 22:12:48 +0100

    Oops

--- buildscript/src/main/java/Versions.java
+++ buildscript/src/main/java/Versions.java
@@ -1,5 +1,4 @@
 public class Versions {
-<<<<<<< HEAD
     public static final String MINECRAFT_VERSION = "1.8.9";
     public static final String YARN_VERSION = "1.8.9+build.202203281833";
     public static final String LOADER_VERSION = "0.13.3";
