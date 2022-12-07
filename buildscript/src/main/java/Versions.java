e8183b49ef9f66df897be7b84e5b544b644128cf
ZtereoHYPE <57519662+ZtereoHYPE@users.noreply.github.com>
2022-12-07 18:51:23 +0100

    change: update to 1.19.3

--- buildscript/src/main/java/Versions.java
+++ buildscript/src/main/java/Versions.java
@@ -1,7 +1,7 @@
 public class Versions {
-    public static final String MINECRAFT_VERSION = "1.19.2";
-    public static final String YARN_VERSION = "1.19.2+build.1";
-    public static final String LOADER_VERSION = "0.14.9";
+    public static final String MINECRAFT_VERSION = "1.19.3";
+    public static final String YARN_VERSION = "1.19.3+build.1";
+    public static final String LOADER_VERSION = "0.14.10";
     public static final String QUILTFLOWER_VERSION = "1.8.1";
     public static final String LAZYDFU_VERSION = "0.1.3";
     public static final String YAML_VERSION = "1.15";
