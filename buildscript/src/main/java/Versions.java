f3db57deea2460e3d7ba6075463032a20f04ea76
DragonEggBedrockBreaking <68545280+DragonEggBedrockBreaking@users.noreply.github.com>
2023-07-17 22:43:48 +0100

    Update to 1.20 (1.19 version doesn't work for some reason)

--- buildscript/src/main/java/Versions.java
+++ buildscript/src/main/java/Versions.java
@@ -1,9 +1,8 @@
 public class Versions {
-    public static final String MINECRAFT_VERSION = "1.19.3";
-    public static final String YARN_VERSION = "1.19.3+build.1";
-    public static final String LOADER_VERSION = "0.14.10";
+    public static final String MINECRAFT_VERSION = "1.20.1";
+    public static final String YARN_VERSION = "1.20.1+build.9";
+    public static final String LOADER_VERSION = "0.14.21";
     public static final String QUILTFLOWER_VERSION = "1.9.0";
-    public static final String LAZYDFU_VERSION = "0.1.3";
     public static final String YAML_VERSION = "1.15";
     public static final int JAVA_VERSION = 17;
 }
